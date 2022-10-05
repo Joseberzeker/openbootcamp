7742f2ef21fbf8a9ccc9e379435199cffb27a6f8
Jose Ahumada <jlan3.1997@gmail.com>
2022-10-05 11:36:30 -0500

    Actualizando cosas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int resultadoSuma = suma(1, 2, 3);
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuertas();
+        System.out.println("El resultado de la suma es :" + resultadoSuma);
+        System.out.println("El número de puertas que tiene el coche es: " + miCoche.numPuertas);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class Coche {
+    public int numPuertas = 0;
+
+    Coche() {
+    }
+
+    public void AgregarPuertas() {
+        ++this.numPuertas;
     }
 }
